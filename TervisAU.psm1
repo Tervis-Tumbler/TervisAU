a987d4750b0294b0f0d4be6e0a953970302c9665
ChrisMagnuson <hourglasssand@gmail.com>
2017-07-29 23:00:35 -0400

    Add Invoke-AUAppveyorBuild

---
+++ TervisAU.psm1
@@ -0,0 +1,15 @@
+﻿function Invoke-AUAppveyorBuild {
+    param (
+        [Parameter(Mandatory,ValueFromPipelineByPropertyName)]$Name,
+        [Parameter(Mandatory,ValueFromPipelineByPropertyName)]$Version
+    )
+    begin {
+        $PackageVersionStrings = @()
+    }
+    process {
+        $PackageVersionStrings += "$Name$(if($Version){":$Version"})"
+    }
+    end {
+        git commit -m "[AU $($PackageVersionStrings -join " ")]" --allow-empty
+    }
+}
